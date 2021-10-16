c30cb305e9c5f84a35fe9bc86cd9f9fb351a374b
Michael Krumdick <mrkrumdick@gmail.com>
2021-10-16 15:43:42 +0000

    Adding an actual training loop with logging

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import os
+import time
 import itertools
+import argparse
 
+from collections import deque
+
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
+from torch.distributions.categorical import Categorical
 
 _pieces = [
     "r",
@@ -59,9 +66,6 @@ class TokenizedChess(Dataset):
         return board_tokens, action
 
 
-from torch.distributions.categorical import Categorical
-
-
 class Model(nn.Module):
     def __init__(self, device, emb_dim):
         super().__init__()
@@ -74,23 +78,23 @@ class Model(nn.Module):
         self.start_token = nn.Parameter(torch.zeros(1, 1, emb_dim))
 
         self.transformer = nn.Transformer(
-            d_model=emb_dim,
-            num_encoder_layers=1,
-            num_decoder_layers=1,
             batch_first=True,
         )
 
         self.to_dist = nn.Sequential(nn.Linear(emb_dim, len(MOVE_TOKENS)))
 
     def forward(self, state, action):
+        state = state.to(self.device)
+        action = action.to(self.device)
+
         embed_state = self.board_embed(state)
         pos_embed_state = embed_state + self.pos_encoding
         tgt_embed = self.move_embed(action)
 
-        repeat_token = self.start_token.repeat(action.shape[0], 1, 1)
+        repeat_token = self.start_token.repeat(action.shape[0], 1, 1).to(self.device)
         tgt_embed_token = torch.cat((repeat_token, tgt_embed), dim=1)
 
-        tgt_mask = self.transformer.generate_square_subsequent_mask(3)
+        tgt_mask = self.transformer.generate_square_subsequent_mask(3).to(self.device)
 
         out = self.transformer(pos_embed_state, tgt_embed_token, tgt_mask=tgt_mask)
         out = out[:, :2]
@@ -98,14 +102,16 @@ class Model(nn.Module):
         return logits
 
     def inference(self, state):
+        state = state.to(self.device)
         embed_state = self.board_embed(state)
         pos_embed_state = embed_state + self.pos_encoding
 
-        decode = self.start_token.repeat(state.shape[0], 1, 1)
-        tokens = torch.zeros((state.shape[0], 1))
+        decode = self.start_token.repeat(state.shape[0], 1, 1).to(self.device)
+        tokens = torch.zeros((state.shape[0], 1)).to(self.device)
 
         for i in range(2):
-            tgt_mask = self.transformer.generate_square_subsequent_mask(i + 1)
+            tgt_mask = self.transformer.generate_square_subsequent_mask(i + 1).to(self.device)
+
             out = self.transformer(pos_embed_state, decode, tgt_mask=tgt_mask)
             logits = self.to_dist(out)[:, -1, :]
             next_value = Categorical(logits=logits).sample().view(-1, 1)
@@ -115,27 +121,63 @@ class Model(nn.Module):
 
         return tokens[:, 1:]
 
+def main(log_file_path:str):
+    data = TokenizedChess("data.csv")
+    dataloader = DataLoader(data, batch_size=256, drop_last=True)
+    loss_fn = nn.CrossEntropyLoss()
 
-data = TokenizedChess("test.txt")
-test = DataLoader(data, batch_size=16, drop_last=True)
-loss_fn = nn.CrossEntropyLoss()
+    device = torch.device("cuda")
+    model = Model(device, 512)
+    model = model.to(model.device)
 
-model = Model(torch.device("cpu"), 16)
+    opt = optim.Adam(model.parameters(), lr=1e-4)
 
-opt = optim.Adam(model.parameters(), lr=1e-2)
+    log_file = open(log_file_path, "w")
 
-for s, a in test:
-    for _ in range(10000):
-        labels = torch.flatten(a)
-        infer = model.inference(s)
-        acc = torch.mean((infer.flatten() == labels).float())
-        print(acc)
+    losses = deque(maxlen=100)
 
-        opt.zero_grad()
-        p = model(s, a)
-        p = p.view(-1, len(MOVE_TOKENS))
-        loss = loss_fn(p, labels)
-        loss.backward()
-        print(loss)
-        opt.step()
-        print()
+    for epoch in range(100):
+        for (batch_idx, (s, a)) in enumerate(dataloader):
+            s = s.to(device)
+            labels = torch.flatten(a).to(device)
+        
+            opt.zero_grad()
+            p = model(s, a)
+            p = p.view(-1, len(MOVE_TOKENS))
+            loss = loss_fn(p, labels)
+            loss.backward()
+            opt.step()
+    
+            loss = loss.item()
+            losses.append(loss)
+    
+            acc = None
+            if batch_idx % 100 == 0:
+                infer = model.inference(s)
+                acc = torch.mean((infer.flatten() == labels).float()).item()
+                print(f'({epoch}/{batch_idx}) Loss (Avg): {np.mean(losses)} Acc: {acc}')
+
+            if batch_idx % 1000 == 0:
+                torch.save(model.state_dict(), "model.pth")
+                torch.save(opt.state_dict(), "opt.pth")
+    
+            log_file.write(f"{epoch} {batch_idx} {time.time()} {loss} {acc}\n")
+
+        torch.save(model.state_dict(), f"model_{epoch}.pth")
+        torch.save(opt.state_dict(), f"opt_{epoch}.pth")
+
+        lt.__exit__()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("log_file")
+    args = parser.parse_args()
+    
+    if os.path.exists(args.log_file):
+        res = input(f"File {args.log_file} exists. Overwrite? (Y/n) ")
+        go_ahead = res == "" or res.lower() == "y"
+
+    if not os.path.exists(args.log_file) or go_ahead:
+        main(args.log_file)
+
