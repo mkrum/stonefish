7261a1efdcda1ee6c73a3974b43735e55c5524e4
Michael Krumdick <mrkrumdick@gmail.com>
2021-11-03 10:51:15 -0400

    Updating the dataset to use the new rep, adding test

--- stonefish/dataset.py
+++ stonefish/dataset.py
@@ -1,55 +1,13 @@
 """
-Simple datase for the tokenized chess data
+Simple pytorch dataset for the chess data
 """
-import torch
-import chess
-from torch.utils.data import Dataset, DataLoader
 
-from constants import MOVE_TOKENS, BOARD_TOKENS, BTOKEN_ID, MTOKEN_ID
+from torch.utils.data import Dataset
 
-def board_to_list(board):
-    builder = []
+from stonefish.rep import BoardRep, MoveRep
 
-    for square in chess.SQUARES_180:
-        piece = board.piece_at(square)
+class ChessData(Dataset):
 
-        if piece:
-            builder.append(piece.symbol())
-        else:
-            builder.append("e")
-	
-    if board.turn == chess.WHITE:
-        builder.append("w")
-    else:
-        builder.append("b")
-
-    return builder
-
-def board_to_tensor(board):
-    tokens = board_to_list(board)
-    tensor = tokenize_board(tokens)
-    return tensor
-
-def tensor_to_move_str(move_tensor):
-    return MOVE_TOKENS[int(move_tensor[0].item())] + MOVE_TOKENS[int(move_tensor[1].item())]
-
-def tensor_to_move(move_tensor):
-    str_value = tensor_to_move_str(move_tensor)
-    try:
-        move = chess.Move.from_uci(str_value)
-    except ValueError:
-        move = chess.Move.from_uci("0000")
-    return move
-
-def tokenize_board(board_state):
-    board_tokens = torch.LongTensor(list(map(BTOKEN_ID.__getitem__, board_state)))
-    return board_tokens
-
-def tokenize_action(actions):
-    action_tokens = torch.LongTensor(list(map(MTOKEN_ID.__getitem__, actions)))
-    return action_tokens
-
-class TokenizedChess(Dataset):
     def __init__(self, path):
         super().__init__()
 
@@ -61,8 +19,11 @@ class TokenizedChess(Dataset):
 
     def __getitem__(self, i):
         raw = self.data[i].rstrip().split(",")
-        board_state = raw[:65]
-        actions = raw[65:]
-        board_tokens = tokenize_board(board_state)
-        action = tokenize_action(actions)
-        return board_tokens, action
+
+        # Data is "fen,move"
+        board_fen = raw[0]
+        actions = raw[1]
+
+        board_tokens = BoardRep.from_fen(board_fen)
+        move = MoveRep.from_str(actions)
+        return board_tokens.to_tensor(), move.to_tensor()
