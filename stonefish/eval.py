695e100050c08ba620f01b3ba546ca7b948daca5
Michael Krumdick <mrkrumdick@gmail.com>
2021-11-05 18:23:47 -0400

    More visualizations

--- stonefish/eval.py
+++ stonefish/eval.py
@@ -8,11 +8,13 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 
 from stonefish.model import Model
 from stonefish.dataset import ChessData
-from stonefish.rep import MoveToken
+from stonefish.rep import MoveToken, MoveRep, BoardRep
+from stonefish.vis import plot_board, square_to_grid, plot_move, mark_move
 
 from rich.progress import track
 
@@ -30,6 +32,7 @@ def eval_model(model, data, batch_size=1024, max_batch=20):
         enumerate(dataloader), "Testing...", total=max_batch
     ):
         infer = model.inference(s)
+
         infer = torch.flatten(infer)
         labels = torch.flatten(a).to(infer.device)
 
@@ -55,15 +58,89 @@ def main(dataset, load_model):
 
     data = ChessData(dataset)
 
-    device = torch.device("cuda")
-    model = Model(device, 512)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = Model(device, 128)
     model = model.to(model.device)
+    print(model.load_state_dict(torch.load(load_model, map_location=device)))
 
-    print(model.load_state_dict(torch.load(load_model)))
-
-    acc, m_loss = eval_model(model, data)
+    acc, m_loss = eval_model(model, data, batch_size=16)
     print(acc)
     print(m_loss)
+
+
+def vis(dataset, load_model):
+    data = ChessData(dataset)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = Model(device, 128)
+    model = model.to(model.device)
+    model.load_state_dict(torch.load(load_model, map_location=device))
+
+    state, action = data[np.random.choice(range(len(data)))]
+
+    state = state.unsqueeze(0)
+    action = action.unsqueeze(0)
+
+    with torch.no_grad():
+        out = model.forward(state, action)
+
+    act = nn.Softmax(dim=-1)
+    for i in range(2):
+
+        first_logits = out[0, i]
+
+        probs = act(first_logits).numpy()
+
+        prob_grid = np.zeros((8, 8))
+        for (square, p) in zip(MoveToken.valid_str(), probs):
+            x, y = square_to_grid(square)
+            prob_grid[x, y] += p
+
+        fig, ax = plt.subplots(1, 1)
+
+        plot_board(ax, BoardRep.from_tensor(state[0]).to_board(), checkers=False)
+        ax.imshow(prob_grid, vmax=1.0, vmin=0.0, cmap="Reds")
+        plt.show()
+
+
+def better_vis(dataset, load_model):
+    data = ChessData(dataset)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = Model(device, 128)
+    model = model.to(model.device)
+    model.load_state_dict(torch.load(load_model, map_location=device))
+
+    state, action = data[np.random.choice(range(len(data)))]
+
+    board = BoardRep.from_tensor(state).to_board()
+    move = MoveRep.from_tensor(action).to_uci()
+
+    state = state.unsqueeze(0)
+
+    legal_moves = list(board.legal_moves)
+
+    action_stack = torch.stack([MoveRep.from_uci(m).to_tensor() for m in legal_moves])
+    state = state.repeat(action_stack.shape[0], 1)
+
+    act = nn.Softmax(dim=0)
+    with torch.no_grad():
+        out = model.forward(state, action_stack)
+
+        logits = torch.zeros(out.shape[0])
+        for (i, o) in enumerate(out):
+            logits[i] += o[0, action_stack[i, 0]] + o[1, action_stack[i, 1]]
+
+    probs = act(logits)
+
+    fig, ax = plt.subplots(1, 1)
+
+    plot_board(ax, board, checkers=True)
+    mark_move(ax, move)
+    for i in range(len(legal_moves)):
+        plot_move(ax, board, legal_moves[i], alpha=probs[i].item())
+
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -73,4 +150,4 @@ if __name__ == "__main__":
     parser.add_argument("load_model")
     args = parser.parse_args()
 
-    main(args.dataset, args.load_model)
+    better_vis(args.dataset, args.load_model)
--- stonefish/vis.py
+++ stonefish/vis.py
@@ -1,38 +1,67 @@
-
 import chess
 import matplotlib.pyplot as plt
 import numpy as np
+from typing import Tuple
 
 import stonefish.utils as ut
+from stonefish.rep import MoveRep
+import matplotlib.patches as patches
 
 symbols = {
-    'K': '♔',
-    'Q': '♕',
-    'R': '♖',
-    'B': '♗',
-    'N': '♘',
-    'P': '♙',
-    'k': '♚',
-    'q': '♛',
-    'r': '♜',
-    'b': '♝',
-    'n': '♞',
-    'p': '♟︎',
+    "K": "♔",
+    "Q": "♕",
+    "R": "♖",
+    "B": "♗",
+    "N": "♘",
+    "P": "♙",
+    "k": "♚",
+    "q": "♛",
+    "r": "♜",
+    "b": "♝",
+    "n": "♞",
+    "p": "♟︎",
 }
 
+
 def make_board(ax):
-    """ 
+    """
     Adds checkers to the board to make it look more natural
     """
     X, Y = np.meshgrid(np.arange(8), np.arange(8))
     checker = (((X + Y) % 2) + 0.3) / 2
     ax.imshow(checker, cmap="Greys", vmax=1.0, vmin=0.0)
 
-def add_piece(ax, x, y, piece):
+
+def add_piece(ax, x, y, piece, alpha=1.0, color="black"):
     """
     Adds a pieces to the board, x and y should be the war x,y coords
     """
-    ax.text(x, y + 0.05, symbols[piece], fontsize=32, ha='center', va='center')
+    ax.text(
+        x,
+        y + 0.05,
+        symbols[piece],
+        fontsize=32,
+        ha="center",
+        va="center",
+        alpha=alpha,
+        color=color,
+    )
+
+
+def add_arrow(ax, from_x, from_y, to_x, to_y, alpha=1.0, color="black"):
+    """
+    Adds a pieces to the board, x and y should be the war x,y coords
+    """
+    ax.arrow(
+        from_x,
+        from_y + 0.05,
+        to_x - from_x,
+        to_y - from_y,
+        alpha=alpha,
+        color=color,
+        head_width=0.1,
+    )
+
 
 def plot_board(ax, board, checkers=True):
     """
@@ -40,13 +69,16 @@ def plot_board(ax, board, checkers=True):
     """
     ax.set_xlim([-0.5, 7.5])
     ax.set_ylim([7.5, -0.5])
+    for i in range(8):
+        ax.axhline(i - 0.5, 0, 8, color="black")
+        ax.axvline(i - 0.5, 0, 8, color="black")
 
     ax.tick_params(labeltop=True, labelright=True, length=0)
-    
+
     ax.set_yticks(list(range(8)))
     ax.set_xticks(list(range(8)))
-    ax.set_yticklabels(('8', '7', '6', '5', '4', '3', '2', '1'))
-    ax.set_xticklabels(('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'))
+    ax.set_yticklabels(("8", "7", "6", "5", "4", "3", "2", "1"))
+    ax.set_xticklabels(("a", "b", "c", "d", "e", "f", "g", "h"))
 
     if checkers:
         make_board(ax)
@@ -59,9 +91,64 @@ def plot_board(ax, board, checkers=True):
         if piece:
             add_piece(ax, locs[i][0], locs[i][1], piece.symbol())
 
-if __name__ == '__main__':
+
+def plot_move(ax, board: chess.Board, move: chess.Move, alpha=1.0, color="black"):
+    move = MoveRep.from_uci(move)
+
+    from_square = move.to_str_list()[0]
+    to_square = move.to_str_list()[1]
+
+    square = chess.SQUARE_NAMES.index(from_square)
+    piece = board.piece_at(square)
+
+    from_col, from_row = square_to_grid(from_square)
+    to_col, to_row = square_to_grid(to_square)
+
+    add_arrow(ax, from_row, from_col, to_row, to_col, alpha=alpha, color=color)
+    add_piece(ax, to_row, to_col, piece.symbol(), alpha=alpha, color=color)
+
+
+def square_to_grid(square: str) -> Tuple[int, int]:
+    row_val = square[0]
+    col_val = square[1]
+    col = ["8", "7", "6", "5", "4", "3", "2", "1"].index(col_val)
+    row = ["a", "b", "c", "d", "e", "f", "g", "h"].index(row_val)
+    return col, row
+
+
+def mark_square(ax, square: str):
+
+    row, col = square_to_grid(square)
+
+    rect = patches.Rectangle(
+        (col - 0.5, row - 0.5),
+        1.0,
+        1.0,
+        linewidth=2,
+        edgecolor="r",
+        facecolor="none",
+        zorder=3,
+    )
+
+    # Add the patch to the Axes
+    ax.add_patch(rect)
+
+
+def mark_move(ax, move):
+    move = MoveRep.from_uci(move)
+
+    from_square = move.to_str_list()[0]
+    to_square = move.to_str_list()[1]
+    mark_square(ax, from_square)
+    mark_square(ax, to_square)
+
+
+if __name__ == "__main__":
     board = ut.randomize_board()
-    fig, ax = plt.subplots(1,1)
-    plot_board(ax, board, checkers=False)
-    ax.imshow(np.random.rand(8, 8), alpha=0.2, cmap='hot_r')
+    move = np.random.choice(list(board.legal_moves))
+    fig, ax = plt.subplots(1, 1)
+
+    plot_board(ax, board, checkers=True)
+    plot_move(ax, board, move)
+    mark_square(ax, "e1")
     plt.show()
