f51fdc7e0a508242c44f374ef5c968fa520520dc
Michael Krumdick <mrkrumdick@gmail.com>
2022-05-16 12:19:59 -0400

    Turning it into a redis based script as well

--- dataset/parse_pgn.jl
+++ dataset/parse_pgn.jl
@@ -1,65 +1,163 @@
+
+
+using Redis
+using ArgParse
+using Distributed
+
 using Chess
 using Chess.PGN
 using DataFrames
 using JLD2
+ 
+"""
+    pop_chunk_from_list(conn::RedisConnection, list::Symbol,
+    chunk_size::Int)::Vector{String}
 
-function main()
-    file = open("lichess_db_standard_rated_2016-09.pgn", "r")
-    
-    events::Vector{String} = []
-    white_elos::Vector{Int} = []
-    black_elos::Vector{Int}  = []
-    results::Vector{String} = []
-    moves::Vector{String} = []
-    
-    i = 0
+Pops a chunk of data of size `chunk_size` from a list named `list` stored on
+our redis server we are connected to with `conn`.
+"""
+function pop_chunk_from_list(
+    conn::RedisConnection,
+    list::Symbol,
+    chunk_size::Int,
+)::Vector{String}
+
+    pipeline = open_pipeline(conn)
+    # Gets values a chunk_sized subset of our list
+    lrange(pipeline, list, 0, (chunk_size - 1))
+    # Removes that same portion
+    ltrim(pipeline, list, chunk_size, -1)
+
+    data = read_pipeline(pipeline)[1]
+    return data
+end
+
+function load_games(path::String)::Vector{String}
+
+    conn = RedisConnection()
+
+    file = open(path, "r")
+    games::Vector{String} = []
+    buffer::String = ""
+
+    line = readline(file; keep=true)
+    while line != nothing
+        if line[1] == '['
+            buffer = buffer * line
+        end
+
+        if line[1] == '1' 
+            buffer = buffer * line
+            push!(games, buffer)
+            buffer = ""
+        end
+        
+        if length(line) > 2
+            if (line[2] == '1') | (line[2] == '0')
+                buffer = buffer * line
+                push!(games, buffer)
+                buffer = ""
+            end
+        end
+        
+        if length(games) == 1e6
+            lpush(conn, :games, games)
+            games = []
+        end
+
+        line = readline(file; keep=true)
+    end
+    lpush(conn, :games, games)
+end
+
+function parse_game(chunk::Int=100000)
+
+    conn = RedisConnection()
+
+    n = 0
+    game_strs::Vector{String} = []
     while true
-        reader = PGNReader(file)
-        game = readgame(reader; annotations=true)
-        readline(file)
 
-        if headervalue(game, "Termination") != "Normal"
-            continue
+        game_strings = pop_chunk_from_list(conn, :games, chunk)
+
+        if length(game_strings) == 0
+            println(n)
+            return
         end
 
-        event = headervalue(game, "Event")
+        for game_string ∈ game_strings
+        
+            game = gamefromstring(game_string; annotations=true)
 
-        if event == "?"
-            break
-        end
+            if headervalue(game, "Termination") != "Normal"
+                continue
+            end
 
-        if (length(event) < 15) | (event[1:15] != "Rated Classical")
-            continue
-        end
+            try
+                event = headervalue(game, "Event")
+                white_elo = headervalue(game, "WhiteElo")
+                black_elo = headervalue(game, "BlackElo")
+                result = headervalue(game, "Result")
+                move_str = string(game)[10:end]
 
-        try
-            white_elo = parse(Int, headervalue(game, "WhiteElo"))
-            black_elo = parse(Int, headervalue(game, "BlackElo"))
-            result = headervalue(game, "Result")
-            move_str = string(game)[10:end]
+                game_str = move_str * "," * result * "," * white_elo * "," * black_elo *","  * event
+                push!(game_strs, game_str)
 
-            push!(events, event)
-            push!(white_elos, white_elo)
-            push!(black_elos, black_elo)
-            push!(results, result)
-            push!(moves, move_str)
-            i += 1
-        catch
-            println("Missing data")
-        end
-
-        if i % 1000 == 0
-            println(i)
-
-            df = DataFrame(event=events, move=moves, result=results, white_elo=white_elos, black_elo=black_elos)
-    
-            jldopen("df.jld2", "w") do file
-                file["df"] = df
+                if length(game_strs) == 1_000
+                    lpush(conn, :data, game_strs)
+                    game_strs = []
+                end
+            catch
+                println("Missing data")
             end
         end
     end
-
-    df = DataFrame(event=events, move=moves, result=results, white_elo=white_elos, black_elo=black_elos)
-    
-    df
+    lpush(conn, :data, game_strs)
 end
+
+function to_dataset(file_path::String, chunk::Int=100000)
+
+    conn = RedisConnection()
+
+    file = open(file_path, "w")
+
+    while true
+        game_strings = pop_chunk_from_list(conn, :data, chunk)
+
+        if length(game_strings) == 0
+            return
+        end
+
+        for g in game_strings
+            write(file, g * "\n")
+        end
+    end
+end
+
+function main()
+
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "--load"
+        help = ".pgn file to load into the job queue"
+        "--parse"
+        help = "Runs the worker process for the PGN -> FEN processing"
+        action = :store_true
+        "--to_dataset"
+        help = "Converts the data into a dataset"
+    end
+    args = parse_args(s)
+
+    file = args["load"]
+    output_file = args["to_dataset"]
+
+    if file != nothing
+        load_games(file)
+    elseif args["parse"]
+        parse_game()
+    elseif output_file != nothing
+        to_dataset(output_file)
+    end
+end
+
+main()
