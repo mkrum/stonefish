e32b4894588404123d22381513869ed7aa9f2cac
Michael Krumdick <mrkrumdick@gmail.com>
2025-08-02 20:38:30 -0400

    making torchrun work on one proc

--- stonefish/train/base.py
+++ stonefish/train/base.py
@@ -81,10 +81,9 @@ def setup_distributed():
     local_rank = int(os.environ.get("LOCAL_RANK", 0))
     world_size = int(os.environ.get("WORLD_SIZE", 1))
 
-    if world_size > 1:
-        dist.init_process_group(backend="nccl" if torch.cuda.is_available() else "gloo")
-        if torch.cuda.is_available():
-            torch.cuda.set_device(local_rank)
+    dist.init_process_group(backend="nccl" if torch.cuda.is_available() else "gloo")
+    if torch.cuda.is_available():
+        torch.cuda.set_device(local_rank)
         return local_rank, world_size, True
 
     return 0, 1, False
